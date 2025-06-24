02a0e8e64f55f16137df3237b22117b3ada0c85b
charles.hanna <16294731+charlesRepo@users.noreply.github.com>
2025-06-24 16:58:58 -0400

    fix github pages issues

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const isExport = process.env.NEXT_EXPORT === 'true';
+
 const nextConfig = {
-  output: 'export',
+  ...(isExport ? { output: 'export' } : {}),
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  basePath: isProd ? '/meme-generator' : '',
+  assetPrefix: isProd ? '/meme-generator/' : '',
 }
 
 module.exports = nextConfig
